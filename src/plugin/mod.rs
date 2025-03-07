44809fcc1be2d725fb934553c3520ecc8986b3b4
sacha-renault <sacha35340@gmail.com>
2025-03-07 15:59:34 +0100

    feat: display some info about the simulation on the bottom of the app

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -8,13 +8,23 @@ pub struct SysPlugin;
 impl Plugin for SysPlugin {
     fn build(&self, app: &mut App) {
         app
+            // First insert the resources
             .insert_resource(CameraState::default())
             .insert_resource(SimulationState::default())
-            .add_systems(Startup, setup_plugin)
+
+            // Add setup and post setup systems
+            .add_systems(Startup, (setup_plugin, setup_text_visual))
             .add_systems(PostStartup, setup_camera)
-            .add_systems(Update, (setup_body_visuals, update_body_visuals))
+
+            // System for update body position and the visual 
             .add_systems(Update, update_bodies)
+
+            // System for updating ui
             .add_systems(Update, update_camera_position)
+            .add_systems(Update, update_text_visual)
+            .add_systems(Update,  (setup_body_visuals, update_body_visuals))
+
+            // System for handling user input
             .add_systems(Update, 
                 (handle_wheel_event, handle_keyboard_event, handle_mouse_motion_event));
     }
--- src/plugin/systems/ui.rs
+++ src/plugin/systems/ui.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::shared::Body;
+use crate::shared::{components::any::BottomText, Body, CameraState, SimulationState};
 
 // This system updates the visuals for bodies when they move
 pub fn update_body_visuals(
@@ -10,5 +10,42 @@ pub fn update_body_visuals(
         // Update the position to match the body's physics position
         transform.translation.x = body.get_position().x;
         transform.translation.y = body.get_position().y;
+    }
+}
+
+pub fn setup_text_visual(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Load a font
+    // let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+    
+    // Spawn the text bundle
+    commands.spawn((
+        Text::new(""),
+        TextFont {
+            font_size: 15.,
+            ..default()
+        } ,
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.0),
+            left: Val::Px(5.0),
+            ..default()
+        },
+        BottomText,
+    ));
+}
+
+pub fn update_text_visual(
+    game_state: Res<SimulationState>,
+    camera_state: Res<CameraState>,
+    mut query: Query<&mut Text, With<BottomText>>,
+) {
+    if let Ok(mut text) = query.get_single_mut() {
+        let next_text = format!(
+            "Center : ({}, {}) ; Scale : {} ; Time factor : {}",
+            camera_state.position().x, camera_state.position().y,
+            camera_state.scale(),
+            if !game_state.paused { game_state.time_factor.to_string() } else { String::from("Paused") },
+        );
+        **text = next_text;
     }
 }
